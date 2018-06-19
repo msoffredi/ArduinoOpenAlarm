ca5858bf942912f9d16b740a1dc97e849f40e67b
Marcelo Soffredi <msoffredi@gmail.com>
2018-06-19 17:49:52 -0300

    Minimized Spanish language text version added

---
+++ languages/Language.es.min.h
@@ -0,0 +1,48 @@
+#ifndef LANGUAGE_H
+#define LANGUAGE_H
+
+#define TEXT_INITIALIZING "Inicializando..."
+#define TEXT_INIT_DONE "Listo!"
+
+#define TEXT_SOFTWARE_VERSION "Ver "
+
+#define TEXT_LIST_SENSORS_SENSOR_ROW "Sensor #"
+#define TEXT_LIST_SENSORS_STATUS_TEXT ", estado: "
+#define TEXT_LIST_SENSORS_STATUS_ON "on"
+#define TEXT_LIST_SENSORS_STATUS_OFF "off"
+#define TEXT_LIST_SENSORS_NO_SENSORS "No hay sensores"
+
+#define TEXT_ALARM_STATUS "Estado Alarma: "
+#define TEXT_ALARM_ARMED "Alarma armada"
+#define TEXT_ALARM_DISARMED "Alarma desarmada"
+#define TEXT_ALARM_NOT_ARMED_SENSORS_ACTIVE "Alarma no armada. Sensor activo"
+
+#define TEXT_SENSOR_ADDED "Agregado #"
+#define TEXT_SENSOR_ADDED_PIN ". P"
+#define TEXT_SENSOR_DELETED "Borrado #"
+#define TEXT_SENSOR_NOT_ADDED_BLACKLIST "No agregado. Pin prohibido"
+#define TEXT_SENSOR_NOT_ADDED_USED_PIN "No agregado. Pin usado"
+
+#define TEXT_ENTER_ADMIN_MODE "Modo Admin on"
+#define TEXT_EXIT_ADMIN_MODE "Modo admin off"
+#define TEXT_ADMIN_USER_CODE_CHANGED "Codigo cambiado"
+#define TEXT_ADMIN_ADMIN_CODE_CHANGED "Codigo cambiado"
+
+#define TEXT_LIST_SENSORS_ID ", ID "
+#define TEXT_LIST_SENSORS_S2_ID ", E2 ID "
+#define TEXT_LIST_SENSORS_PIN ", P"
+
+// Wireless RF texts 
+#define TEXT_WIRELESS_LEARNING "Modo aprendizaje. inalambrico..."
+#define TEXT_WIRELESS_LEARNING2_1 "Modo aprendizaje. inalambrico 2..."
+#define TEXT_WIRELESS_LEARNING2_2 "Esperando. estado 2..."
+#define TEXT_WIRELESS_LEARNING2_3 "Esperando. estado 1 fin"
+#define TEXT_WIRELESS_SENSOR_S1_FAIL "ID estado 1. no concuerda"
+#define TEXT_WIRELESS_SENSOR_ADDED "Agregado #"
+#define TEXT_WIRELESS_SENSOR_EXISTS "Sensor ya existe"
+#define TEXT_WIRELESS_TIMEOUT_LEARNING "Tiempo finalizo!"
+
+#define TEXT_ADMIN_SENSOR_ON_OFF_1 "#"
+#define TEXT_ADMIN_SENSOR_ON_OFF_2 " fue cambiado. a "
+
+#endif /* LANGUAGE_H */
